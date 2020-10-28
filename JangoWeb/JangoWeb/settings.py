ec981f46c2a05a7a859c86ab6c002961d213b85b
DeathGodBXX <2694779216@qq.com>
2020-10-28 10:40:33 +0800

    connect mysql in linux

--- JangoWeb/JangoWeb/settings.py
+++ JangoWeb/JangoWeb/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'JangoWeb.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '192.168.59.40',
+        'PORT': 3306,
+        'USER': 'root',
+        'PASSWORD': '1993e4d5f',
+        'NAME': 'py_mysql',
     }
 }
 
